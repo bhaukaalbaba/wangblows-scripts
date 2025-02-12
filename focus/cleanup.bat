5e2852658466ed788b4acd807771981c2c132bc2
Bhaukaal Baba <47282314+bhaukaalbaba@users.noreply.github.com>
2025-02-13 03:00:40 +0530

    Create cleanup.bat
    First version

---
+++ focus/cleanup.bat
@@ -0,0 +1,78 @@
+@echo off
+setlocal
+
+:: Define log file name with date and time
+set mydate=%date:/=-%
+set mytime=%time::=-%
+set "logFile=D:\cleanuplogs\cleanup-log-%mydate%-%mytime%.log"
+echo Log file: %logFile%
+
+:: Direct logging to see if date and time are set correctly
+echo [%date% %time%] Script started >> %logFile%
+echo Script started
+
+:: Check for admin privileges
+echo Checking for admin privileges...
+echo [%date% %time%] Checking for admin privileges... >> %logFile%
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo [%date% %time%] Requesting administrative privileges... >> %logFile%
+    echo Requesting administrative privileges...
+    echo Please run this script with administrative privileges.
+    pause
+    exit /b
+)
+echo [%date% %time%] Admin privileges confirmed. >> %logFile%
+echo Admin privileges confirmed.
+
+:: Stop IIS service
+echo Stopping IIS service...
+echo [%date% %time%] Stopping IIS service... >> %logFile%
+net stop w3svc >> %logFile% 2>&1
+if '%errorlevel%' NEQ '0' (
+    echo [%date% %time%] Failed to stop IIS service. >> %logFile%
+    echo Failed to stop IIS service.
+    goto :EOF
+)
+echo [%date% %time%] IIS service stopped. >> %logFile%
+echo IIS service stopped.
+
+:: Define directories to clean
+set "dirs=C:\Windows\SystemTemp C:\Windows\Temp C:\Users\Administrator\Local\Temp"
+
+:: Delete files in specified directories
+for %%d in (%dirs%) do (
+    echo Deleting files in %%d...
+    echo [%date% %time%] Deleting files in %%d... >> %logFile%
+    for %%f in (%%d\*) do (
+        del /F /Q "%%f" 2>> %logFile% || (
+            echo [%date% %time%] Could not delete %%f, skipping... >> %logFile%
+            echo Could not delete %%f, skipping...
+        )
+    )
+)
+echo [%date% %time%] File deletion completed. >> %logFile%
+echo File deletion completed.
+
+:: Start IIS service
+echo Starting IIS service...
+echo [%date% %time%] Starting IIS service... >> %logFile%
+net start w3svc >> %logFile% 2>&1
+if '%errorlevel%' NEQ '0' (
+    echo [%date% %time%] Failed to start IIS service. >> %logFile%
+    echo Failed to start IIS service.
+    goto :EOF
+)
+echo [%date% %time%] IIS service started. >> %logFile%
+echo IIS service started.
+
+:: Check IIS service status
+echo Checking IIS service status on ports 80 and 443...
+echo [%date% %time%] Checking IIS service status on ports 80 and 443... >> %logFile%
+netstat -an | findstr ":80 :443" >> %logFile%
+
+echo [%date% %time%] Script ended. >> %logFile%
+echo Script ended.
+
+endlocal
+exit /b
